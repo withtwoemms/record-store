e303be72fae4aab1de0dfead1dcc21971e4d3466
Emmanuel I. Obi <withtwoemms@gmail.com>
2016-06-28 09:58:52 -0500

    assert on the presence of records

--- record_store_spec.rb
+++ record_store_spec.rb
@@ -12,5 +12,8 @@ describe 'RecordStore' do
     it 'should throw a NoRecordsFound error if none present' do
       expect { RecordStore.new(recordless) }.to raise_error(NoRecordsFound)
     end
+    it 'should have records if some found' do
+      expect(record_store.records).not_to be_empty
+    end
   end
 end
