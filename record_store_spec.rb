39efa3e67459ba60c8893c3bb6c1c317890b2b87
Emmanuel I. Obi <withtwoemms@gmail.com>
2016-06-28 09:52:35 -0500

    spec checks whether RecordStore init w records

--- record_store_spec.rb
+++ record_store_spec.rb
@@ -2,10 +2,15 @@ require_relative 'record_store'
 
 describe 'RecordStore' do
   describe '#initialize' do
-    let(:record_store) { RecordStore.new('records.csv') }
+    let(:records) { 'records.csv' }
+    let(:recordless) { 'recordless.csv' }
+    let(:record_store) { RecordStore.new(records) }
 
     it 'should create a RecordStore object' do
       expect(record_store.class).to be(RecordStore)
+    end
+    it 'should throw a NoRecordsFound error if none present' do
+      expect { RecordStore.new(recordless) }.to raise_error(NoRecordsFound)
     end
   end
 end
