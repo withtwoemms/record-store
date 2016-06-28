c828622d43ae88331d61f4f12f2dca123b80be0a
Emmanuel I. Obi <withtwoemms@gmail.com>
2016-06-28 09:28:15 -0500

    add spec asserting on RecordStore#init

--- record_store_spec.rb
+++ record_store_spec.rb
@@ -1,4 +1,11 @@
 require_relative 'record_store'
 
 describe 'RecordStore' do
+  describe '#initialize' do
+    let(:record_store) { RecordStore.new('records.csv') }
+
+    it 'should create a RecordStore object' do
+      expect(record_store.class).to be(RecordStore)
+    end
+  end
 end
