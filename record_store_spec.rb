342692fd6957f162e65fdabd2fcec237af23c8f0
Emmanuel I. Obi <withtwoemms@gmail.com>
2016-06-28 12:49:36 -0500

    use proper expect syntax

--- record_store_spec.rb
+++ record_store_spec.rb
@@ -18,11 +18,10 @@ describe 'RecordStore' do
   end
 
   describe '#add' do
-    # LastName, FirstName, Gender, FavoriteColor, DateOfBirth
     let(:record) { 'McPersonson, Person, F, red, 4/20/1990' }
 
     it 'should increase the number of records by 1' do
-      expect(record_store.add record).to change { record_store.records.count }.by(1)
+      expect { record_store.add record }.to change { record_store.records.count }.by(1)
     end
   end
 end
